f777caf7f9b11bca20b9cf07d245917e6336b682
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2025-11-18 11:05:20 +0100

    chore: update palette

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
+          glow: 'hsl(var(--primary-glow))',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
