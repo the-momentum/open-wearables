2fe95511aaa09eb507340f7d6b8d0a0ed74a4a8a
czajkub <czajkub@gmail.com>
2025-11-27 16:50:46 +0100

    remove unused services and move workout[statistic] services to services/

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -4,10 +4,10 @@ from .api_key_service import ApiKeyDep, api_key_service
 from .apple.apple_xml.presigned_url_service import import_service as pre_url_service
 from .apple.auto_export.import_service import import_service as ae_import_service
 from .apple.healthkit.import_service import import_service as hk_import_service
-from .apple.workout_service import workout_service
-from .apple.workout_statistic_service import workout_statistic_service
 from .suunto_import_service import import_service as suunto_import_service
 from .garmin_import_service import import_service as garmin_import_service
+from .workout_service import workout_service
+from .workout_statistic_service import workout_statistic_service
 from .developer_service import developer_service
 from .services import AppService
 from .user_service import user_service
