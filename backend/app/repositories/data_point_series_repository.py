8195413c37d47902d8a59fe7a8f52e4994a262fa
czajkub <czajkub@gmail.com>
2025-12-11 15:07:39 +0100

    pop key fix

--- backend/app/repositories/data_point_series_repository.py
+++ backend/app/repositories/data_point_series_repository.py
@@ -32,7 +32,7 @@ class DataPointSeriesRepository(
         creation_data = creator.model_dump()
         creation_data["external_device_mapping_id"] = mapping.id
         creation_data["series_type_definition_id"] = get_series_type_id(creator.series_type)
-        for redundant_key in ("user_id", "provider_name", "device_id", "series_type_definition_id"):
+        for redundant_key in ("user_id", "provider_name", "device_id", "series_type"):
             creation_data.pop(redundant_key, None)
 
         creation = self.model(**creation_data)
