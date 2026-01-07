07bb51fe67c878e5cb1b0b06d2b43363e651d52d
czajkub <czajkub@gmail.com>
2026-01-07 13:40:20 +0100

    service import fix for xml

--- backend/app/integrations/celery/tasks/process_upload_task.py
+++ backend/app/integrations/celery/tasks/process_upload_task.py
@@ -7,7 +7,9 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
-from app.services import event_record_service, timeseries_service, user_service
+from app.services import event_record_service
+from app.services.timeseries_service import timeseries_service
+from app.services.user_service import user_service
 from app.services.apple.apple_xml.aws_service import s3_client
 from app.services.apple.apple_xml.xml_service import XMLService
 from celery import shared_task
