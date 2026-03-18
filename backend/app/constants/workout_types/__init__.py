2f202fc1006023a5b957d4ec15c8c690787d3154
Kyle Coelho <kylecoelho98@gmail.com>
2026-03-17 23:23:59 -0700

    style: sort fitbit import alphabetically in workout_types __init__

--- backend/app/constants/workout_types/__init__.py
+++ backend/app/constants/workout_types/__init__.py
@@ -3,11 +3,11 @@ from .apple_sdk import get_activity_name as get_activity_name_apple_sdk
 from .apple_sdk import get_unified_workout_type as get_unified_apple_workout_type_sdk
 from .apple_xml import get_activity_name as get_activity_name_apple_xml
 from .apple_xml import get_unified_workout_type as get_unified_apple_workout_type_xml
+from .fitbit import get_unified_workout_type as get_unified_fitbit_workout_type
 from .garmin import get_unified_workout_type as get_unified_garmin_workout_type
 from .polar import get_unified_workout_type as get_unified_polar_workout_type
 from .strava import get_unified_workout_type as get_unified_strava_workout_type
 from .suunto import get_unified_workout_type as get_unified_suunto_workout_type
-from .fitbit import get_unified_workout_type as get_unified_fitbit_workout_type
 from .whoop import get_unified_workout_type as get_unified_whoop_workout_type
 
 __all__ = [
