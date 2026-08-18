e2ad86a076ec72dd2a48ddd678fd00d93fa9490a
czajkub <czajkub@gmail.com>
2026-08-18 16:47:32 +0200

    migration linting

--- backend/migrations/versions/2026_08_17_1559-194bb4179f1f_sync_run_tracking.py
+++ backend/migrations/versions/2026_08_17_1559-194bb4179f1f_sync_run_tracking.py
@@ -4,15 +4,12 @@ Revision ID: 194bb4179f1f
 Revises: b2c3d4e5f6a1
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '194bb4179f1f'
-down_revision: Union[str, None] = 'b2c3d4e5f6a1'
+revision: str = "194bb4179f1f"
+down_revision: Union[str, None] = "b2c3d4e5f6a1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
