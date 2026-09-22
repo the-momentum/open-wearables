f8a35a3e678716cb46ccf062471ce98e1c6a6388
Artur Lepioszka <artur.lepioszka@themomentum.ai>
2026-09-22 20:47:39 +0200

    Update coverage

--- backend/app/schemas/model_crud/activities/meal.py
+++ backend/app/schemas/model_crud/activities/meal.py
@@ -1,6 +1,8 @@
+from pydantic import Field
+
 from .event_record_detail import EventRecordDetailCreate
 
 
 class MealDetailCreate(EventRecordDetailCreate):
-    title: str | None = None
-    meal_type: str | None = None
+    title: str | None = Field(default=None, max_length=255)
+    meal_type: str | None = Field(default=None, max_length=32)
